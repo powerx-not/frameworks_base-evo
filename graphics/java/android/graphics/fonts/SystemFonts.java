JNWSG/frameworks_base

graphics: Introduce selectable emoji customization
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

graphics/java/android/graphics/fonts/SystemFonts.java
@@ -25,6 +25,7 @@ import android.annotation.Nullable;
 import android.graphics.FontListParser;
 import android.graphics.Typeface;
 import android.os.LocaleList;
+import android.os.SystemProperties;
 import android.ravenwood.annotation.RavenwoodReplace;
 import android.text.FontConfig;
 import android.util.ArrayMap;
@@ -63,6 +64,13 @@ public final class SystemFonts {
     /** @hide */
     public static final String SYSTEM_FONT_DIR = getSystemFontDir();
     private static final String OEM_XML = "/product/etc/fonts_customization.xml";
+    private static final String OEM_EMOJI_XML_IOS = "/product/etc/fonts_customization_emoji_ios.xml";
+    private static final String OEM_EMOJI_XML_SAMSUNG =
+            "/product/etc/fonts_customization_emoji_samsung.xml";
+    private static final String PROP_EMOJI_STYLE = "persist.sys.ax_emoji_style";
+    private static final String EMOJI_STYLE_ANDROID = "android";
+    private static final String EMOJI_STYLE_IOS = "ios";
+    private static final String EMOJI_STYLE_SAMSUNG = "samsung";
     /** @hide */
     public static final String OEM_FONT_DIR = "/product/fonts/";
 
@@ -352,8 +360,9 @@ public final class SystemFonts {
             long lastModifiedDate,
             int configVersion
     ) {
-        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR,
-                updatableFontMap, lastModifiedDate, configVersion);
+        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, updatableFontMap,
+                lastModifiedDate, configVersion);
     }
 
     /**
@@ -368,8 +377,9 @@ public final class SystemFonts {
             long lastModifiedDate,
             int configVersion
     ) {
-        return getSystemFontConfigInternal(fontsXml, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR,
-                updatableFontMap, lastModifiedDate, configVersion);
+        return getSystemFontConfigInternal(fontsXml, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, updatableFontMap,
+                lastModifiedDate, configVersion);
     }
 
     /**
@@ -377,22 +387,23 @@ public final class SystemFonts {
      * @hide
      */
     public static @NonNull FontConfig getSystemPreinstalledFontConfig() {
-        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR, null,
-                0, 0);
+        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, null, 0, 0);
     }
 
     /**
      * @hide
      */
     public static @NonNull FontConfig getSystemPreinstalledFontConfigFromLegacyXml() {
-        return getSystemFontConfigInternal(LEGACY_FONTS_XML, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR,
-                null, 0, 0);
+        return getSystemFontConfigInternal(LEGACY_FONTS_XML, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, null, 0, 0);
     }
 
     /* package */ static @NonNull FontConfig getSystemFontConfigInternal(
             @NonNull String fontsXml,
             @NonNull String systemFontDir,
             @Nullable String oemXml,
+            @Nullable String emojiXml,
             @Nullable String productFontDir,
             @Nullable Map<String, File> updatableFontMap,
             long lastModifiedDate,
@@ -400,8 +411,15 @@ public final class SystemFonts {
     ) {
         try {
             Log.i(TAG, "Loading font config from " + fontsXml);
-            return FontListParser.parse(fontsXml, systemFontDir, oemXml, productFontDir,
-                                                updatableFontMap, lastModifiedDate, configVersion);
+            final List<String> customizationXmls = new ArrayList<>();
+            if (oemXml != null) {
+                customizationXmls.add(oemXml);
+            }
+            if (emojiXml != null) {
+                customizationXmls.add(emojiXml);
+            }
+            return FontListParser.parse(fontsXml, systemFontDir, customizationXmls,
+                    productFontDir, updatableFontMap, lastModifiedDate, configVersion);
         } catch (IOException e) {
             Log.e(TAG, "Failed to open/read system font configurations.", e);
             return new FontConfig(Collections.emptyList(), Collections.emptyList(),
@@ -411,6 +429,20 @@ public final class SystemFonts {
             return new FontConfig(Collections.emptyList(), Collections.emptyList(),
                     Collections.emptyList(), Collections.emptyList(), 0, 0);
         }
+    }
+
+    private static @Nullable String getEmojiCustomizationXml() {
+        final String style = SystemProperties.get(PROP_EMOJI_STYLE, EMOJI_STYLE_IOS);
+        if (EMOJI_STYLE_SAMSUNG.equals(style)) {
+            return OEM_EMOJI_XML_SAMSUNG;
+        }
+        if (EMOJI_STYLE_IOS.equals(style)) {
+            return OEM_EMOJI_XML_IOS;
+        }
+        if (EMOJI_STYLE_ANDROID.equals(style)) {
+            return null;
+        }
+        return OEM_EMOJI_XML_IOS;
     }
 
     /**

powerx-not/frameworks_base-evo

graphics/java/android/graphics/fonts/SystemFonts.java
@@ -25,6 +25,7 @@ import android.annotation.Nullable;
 import android.graphics.FontListParser;
 import android.graphics.Typeface;
 import android.os.LocaleList;
+import android.os.SystemProperties;
 import android.ravenwood.annotation.RavenwoodReplace;
 import android.text.FontConfig;
 import android.util.ArrayMap;
@@ -63,6 +64,13 @@ public final class SystemFonts {
     /** @hide */
     public static final String SYSTEM_FONT_DIR = getSystemFontDir();
     private static final String OEM_XML = "/product/etc/fonts_customization.xml";
+    private static final String OEM_EMOJI_XML_IOS = "/product/etc/fonts_customization_emoji_ios.xml";
+    private static final String OEM_EMOJI_XML_SAMSUNG =
+            "/product/etc/fonts_customization_emoji_samsung.xml";
+    private static final String PROP_EMOJI_STYLE = "persist.sys.ax_emoji_style";
+    private static final String EMOJI_STYLE_ANDROID = "android";
+    private static final String EMOJI_STYLE_IOS = "ios";
+    private static final String EMOJI_STYLE_SAMSUNG = "samsung";
     /** @hide */
     public static final String OEM_FONT_DIR = "/product/fonts/";
 
@@ -352,8 +360,9 @@ public final class SystemFonts {
             long lastModifiedDate,
             int configVersion
     ) {
-        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR,
-                updatableFontMap, lastModifiedDate, configVersion);
+        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, updatableFontMap,
+                lastModifiedDate, configVersion);
     }
 
     /**
@@ -368,8 +377,9 @@ public final class SystemFonts {
             long lastModifiedDate,
             int configVersion
     ) {
-        return getSystemFontConfigInternal(fontsXml, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR,
-                updatableFontMap, lastModifiedDate, configVersion);
+        return getSystemFontConfigInternal(fontsXml, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, updatableFontMap,
+                lastModifiedDate, configVersion);
     }
 
     /**
@@ -377,22 +387,23 @@ public final class SystemFonts {
      * @hide
      */
     public static @NonNull FontConfig getSystemPreinstalledFontConfig() {
-        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR, null,
-                0, 0);
+        return getSystemFontConfigInternal(FONTS_XML, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, null, 0, 0);
     }
 
     /**
      * @hide
      */
     public static @NonNull FontConfig getSystemPreinstalledFontConfigFromLegacyXml() {
-        return getSystemFontConfigInternal(LEGACY_FONTS_XML, SYSTEM_FONT_DIR, OEM_XML, OEM_FONT_DIR,
-                null, 0, 0);
+        return getSystemFontConfigInternal(LEGACY_FONTS_XML, SYSTEM_FONT_DIR, OEM_XML,
+                getEmojiCustomizationXml(), OEM_FONT_DIR, null, 0, 0);
     }
 
     /* package */ static @NonNull FontConfig getSystemFontConfigInternal(
             @NonNull String fontsXml,
             @NonNull String systemFontDir,
             @Nullable String oemXml,
+            @Nullable String emojiXml,
             @Nullable String productFontDir,
             @Nullable Map<String, File> updatableFontMap,
             long lastModifiedDate,
@@ -400,8 +411,15 @@ public final class SystemFonts {
     ) {
         try {
             Log.i(TAG, "Loading font config from " + fontsXml);
-            return FontListParser.parse(fontsXml, systemFontDir, oemXml, productFontDir,
-                                                updatableFontMap, lastModifiedDate, configVersion);
+            final List<String> customizationXmls = new ArrayList<>();
+            if (oemXml != null) {
+                customizationXmls.add(oemXml);
+            }
+            if (emojiXml != null) {
+                customizationXmls.add(emojiXml);
+            }
+            return FontListParser.parse(fontsXml, systemFontDir, customizationXmls,
+                    productFontDir, updatableFontMap, lastModifiedDate, configVersion);
         } catch (IOException e) {
             Log.e(TAG, "Failed to open/read system font configurations.", e);
             return new FontConfig(Collections.emptyList(), Collections.emptyList(),
@@ -411,6 +429,20 @@ public final class SystemFonts {
             return new FontConfig(Collections.emptyList(), Collections.emptyList(),
                     Collections.emptyList(), Collections.emptyList(), 0, 0);
         }
+    }
+
+    private static @Nullable String getEmojiCustomizationXml() {
+        final String style = SystemProperties.get(PROP_EMOJI_STYLE, EMOJI_STYLE_ANDROID);
+        if (EMOJI_STYLE_SAMSUNG.equals(style)) {
+            return OEM_EMOJI_XML_SAMSUNG;
+        }
+        if (EMOJI_STYLE_IOS.equals(style)) {
+            return OEM_EMOJI_XML_IOS;
+        }
+        if (EMOJI_STYLE_ANDROID.equals(style)) {
+            return null;
+        }
+        return OEM_EMOJI_XML_IOS;
     }
 
     /**